285f3beedbccc323bd990c0e1b4feff9ca50d189
Andrea014 <andrea.contrerascarpena@usp.ceu.es>
2026-04-28 17:17:45 +0200

    user and role

--- src/Pojos/Role.java
+++ src/Pojos/Role.java
@@ -1,5 +1,27 @@
 package Pojos;
 
-public class Role {
+import java.io.Serializable;
 
+public class Role implements Serializable {
+
+    private Integer id;
+    private String roleName; // Ej: "admin", "pharmacist", "manager"ç
+    
+    public Role() {}
+
+    public Role(Integer id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getRoleName() { return roleName; }
+    public void setRoleName(String roleName) { this.roleName = roleName; }
+    
+    @Override
+    public String toString() {
+        return roleName;
+    }
 }
--- src/Pojos/User.java
+++ src/Pojos/User.java
@@ -1,5 +1,43 @@
 package Pojos;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private Integer id;
+	private String username;
+	private byte[] password;
+	private Role role;
+	
+	public User () {}
+	
+	public User(Integer id, String username, byte[] password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+	
+	public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public byte[] getPassword() { return password; }
+    public void setPassword(byte[] password) { this.password = password; }
+
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
+    
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", role=" + role.getRoleName() + "]";
+    }
+	
+	
+	
+	
+	
 
 }
+
